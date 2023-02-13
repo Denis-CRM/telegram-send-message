9389129d8737c4b4442f6d72b3e4a0dc1a547ff1
Denis-CRM <61589635+Denis-CRM@users.noreply.github.com>
2023-02-13 09:23:46 +0200

    Update telegram_work.php

--- telegram_work.php
+++ telegram_work.php
@@ -1,3 +1,4 @@
+==================Первы вариант
 <?
 $token = "5921740857:AAFj8M-61w0ce4q4MJWUX2zt64CKIZmK5dE";
 
@@ -15,7 +16,7 @@ curl_close($ch);
 echo $resultQuery;
 
 
-Второй вариант
+===================Второй вариант
 
 
 $token = "5340791844:AAEXXD786InvQrlWHRXykV91USOQSevrPVU";
@@ -27,3 +28,24 @@ $textMessage = urlencode($textMessage);
 $urlQuery = "https://api.telegram.org/bot". $token ."/sendMessage?chat_id=". $chat_id ."&text=" . $textMessage;
 
 $result = file_get_contents($urlQuery);
+
+===================третий вариант
+    
+    Давайте посмотрим код для запросов, с использованием Curl.
+
+$token = "5340791844:AAEXXDduvInvQrlykV91USOQSevrPVU";
+
+$getQuery = array(
+     "chat_id" 	=> 1424625511,
+     "text"  	=> "Новое сообщение из формы",
+     "parse_mode" => "html",
+);
+$ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+$resultQuery = curl_exec($ch);
+curl_close($ch);
+
+echo $resultQuery;
